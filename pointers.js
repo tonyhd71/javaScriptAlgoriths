3d25ec5a696a40243dbb8d35a5a5be105bd5952b
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-19 21:28:03 -0500

    working with only positive numbers so far

--- pointers.js
+++ pointers.js
@@ -1,6 +1,21 @@
 function countUniqueValues(sortedArray) {
-    for (let num in sortedArray) {
-        console.log(num);
+     //i, j-------------->    
+    // 1 1 2 3 3 4 4 5 6 
+    let diffValues = [];
+    if (sortedArray.length > 1) {
+        for (let i = 0; i < sortedArray.length; i++) {
+            for (let j = i + 1; j < sortedArray.length; j++) {
+                if (sortedArray[i] != sortedArray[j]) {
+                    diffValues.push(sortedArray[j]);
+                }
+            }
+        } 
+    } 
+    else if (sortedArray.length == 1) {
+        return 1;
     }
+    return diffValues[diffValues.length - 1];
+    //console.log(diffValues);
+    //return j;
 }
-console.log(countUniqueValues([1,1,1,1,1,2]));
+console.log(countUniqueValues([-2,-1,-1,1]));
