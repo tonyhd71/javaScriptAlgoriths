1f34b21e0aace0d68f7dbcf35513b5923768fdaf
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-23 14:57:07 -0500

    video solution bc i didnt figure this out

--- correspondingFrequency.js
+++ correspondingFrequency.js
@@ -1,38 +1,12 @@
-//[1,2,3,444,7,7,12,12,13]
-// i j
-
-function countUniqueValues(numAll) {
-    if (!Array.isArray(numAll)) {
-        return 'Input must be an array';
+//FIND NUMBER OF UNIQUE VALUES IN ARRAY
+function countUniqueValues(arr) {
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        /* inside this for loop we will compare index array of i to index aray of j*/
+        // arr[i] arr[j] -> what do we want to check?
     }
-    let numUniquelength = numAll.length - 1;
-    let lowestNum = numAll[0];
-    let nextDiffNum;
-    let j;
-    for (let i = 0; i < numUniquelength; i++) {
-        j = i + 1;
-       let diffNum = numAll[j];
-        if (numAll[i] < diffNum) {
-            numAll.splice(numAll.indexOf(numAll[i]), 0, diffNum);
-            j++;
-        }
-        //console.log(numAll);
-        //console.log(`value of i is ${numAll[i]}, value of j is ${numAll[j]}`);
-        /*
-        for (let j = 1; j ; j++) {
-                //[1,1,1,1,2,2]
-                           first numAll[i] that is different from j
-                           this happens first at numAll[5];
-                            swap value at numAll[5], which is 4, 
-                            with next sorted number that comes after
-                            */     
-    }
-    return numAll;
-};
-//console.log(countUniqueValues([1]));
+}
+console.log(countUniqueValues([1,1,1,1,1,2]));
 console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
-/*
 console.log(countUniqueValues([]));
 console.log(countUniqueValues([-2,-1,-1,0,1]));
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
-*/
