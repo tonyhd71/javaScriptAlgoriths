1f928285674cb11ce345ba2ec5930284479981b2
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-27 19:36:43 -0500

    added tutorial clean code version

--- binarySearchRetry.js
+++ binarySearchRetry.js
@@ -34,4 +34,17 @@ function binarySearch(arr, value) {
         return middlePoint = arr[Math.round((rightPointer + leftPointer)/2)];
     }
 }
-console.log(binarySearch([2,4,6,8,10,12,14,16,18,20,22,24,26,28,30], 56));
+console.log(binarySearch([2,5,6,9,13,15,28], 28));
+/*BELOW IS SHORT CLEAN VERSION FROM TUTORIAL
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length -1;
+    let middle = Math.floor((start,end) /2);
+    while(arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) end = middle -1;
+        else start = middle + 1;
+        middle = Math.floor((start,end) /2);
+    }
+    return arr[middle] === elem? middle : -1;
+}
+*/
