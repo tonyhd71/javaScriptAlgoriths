cab8fada8c329668f63dccd80222328597c4450b
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-29 20:09:03 -0500

    selectionSorting

--- sorting.js
+++ sorting.js
@@ -33,16 +33,20 @@ function bubbleSort(arr) {
 }
 */
 function bubbleSort(arr) {
+    let noSwaps;
     for (let i =arr.length ; i > 0; i--) {
+        noSwaps = true;
         for (let j = 0; j < i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 //SWAP
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                noSwaps = false;
             }
         }
+        if (noSwaps) break;
     }
     return arr;
 }
-console.log(bubbleSort([37,45,29,8,12,88,-3]));
+console.log(bubbleSort([8,1,2,3,4,5,6,7]));
