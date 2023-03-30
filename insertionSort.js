c1c080bf86f25161cadb4e36a9d1d3cf77b6f82a
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-30 17:36:15 -0500

    insertion algorithm

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+//we gradually fill the first half of the array by gradualling inserting values one at a  time
+function insertionSort(arr) {
+    return arr;
+    for (let i =1;i <arr.length; i++) {
+        let currentVal = arr[i]; 
+        for (let j = i -1; j >= 0; j-- ) {
+
+        } //set j 1 less to i
+    }
+    return arr;
+}
+insertionSort([2,1,9,76,4])
+//[1.2.9,76,4]
