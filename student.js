f83fcc3826616e98291991bc10459cfbcf9e6778
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-04-07 21:43:01 -0500

    still on methods

--- student.js
+++ student.js
@@ -10,11 +10,15 @@ class Student {
 	}
 	markLate() {
 		this.tardies += 1;
-		return `${this.firstName} ${this.lastName}`;
+		if (this.tardies >= 3) {
+			return 'YOU ARE EXPELLED';
+		}
+		return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
 	}
 	}
 //to create a new instance
-let firstStudent = new Student("Colt", "Steele");
-let secondStudent = new Student("Blue", "Steele");
-console.log(`${jane.fullName()}`);
-console.log(``);
+let firstStudent = new Student("Colt", "Steele", 1);
+let secondStudent = new Student("Blue", "Steele", 3);
+for (let i = 0; i <= 2;i++) {
+	console.log(firstStudent.markLate());
+}
