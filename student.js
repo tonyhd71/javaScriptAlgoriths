2a563143e8f71e905c8d48e413afc768b1a69d08
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-04-07 23:08:32 -0500

    still on methods..

--- student.js
+++ student.js
@@ -17,12 +17,27 @@ class Student {
 		return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
 	}
 	addScore(score) {
+		//the scores of THIS student
 		this.scores.push(score);
+		return this.scores;
 	}
+	calculateAverage() {
+		//reduce() returns the sum of all elements in an array
+		let sum = this.scores.reduce(function(a,b){return a+b;});
+		return sum/this.scores.length;
 	}
+}
 //to create a new instance
 let firstStudent = new Student("Colt", "Steele", 1);
 let secondStudent = new Student("Blue", "Steele", 3);
+secondStudent.addScore(92);
+console.log(`Second student has ${secondStudent.scores}`);
+secondStudent.addScore(66);
+secondStudent.addScore(100);
+console.log(secondStudent.scores);
+console.log('average' + secondStudent.calculateAverage());
+/*
 console.log(firstStudent.markLate());
 console.log(firstStudent.addScore());
+*/
 
