4448bf24aec2c3fb789c1c725ac3a6152fbd7b3a
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-04-28 22:11:34 -0500

    idk

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -38,11 +38,25 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+    shift() {
+        if (this.length === 0) return undefined;
+        let oldHead = this.head;
+        if (this.length === 1) {
+            this.head =null;
+            this.tail = null;
+        }
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+        this.length--;
+        return oldHead;
+    }
 }
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
-list.pop(2);
+list.push(3);
+list.pop();
 console.log(list);
 /*
 let first = new Node(12);
